99d01d3e586981b49cbbf871c4c5d01630967614
Kenny Hung <kenny.hung@oliverwyman.com>
2018-03-16 09:26:02 +0000

    Up to N07

--- src/i_introduction/_2_Named_Arguments/n02NamedArguments.kt
+++ src/i_introduction/_2_Named_Arguments/n02NamedArguments.kt
@@ -7,6 +7,7 @@ import util.doc2
 // default values for arguments
 fun bar(i: Int, s: String = "", b: Boolean = true) {}
 
+
 fun usage() {
     // named arguments
     bar(1, b = false)
@@ -23,7 +24,5 @@ fun todoTask2(): Nothing = TODO(
     documentation = doc2(),
     references = { collection: Collection<Int> -> task1(collection); collection.joinToString() })
 
-fun task2(collection: Collection<Int>): String {
-    todoTask2()
-    return collection.joinToString()
-}
+fun task2(collection: Collection<Int>): String =
+    collection.joinToString(prefix = "{", postfix = "}")
